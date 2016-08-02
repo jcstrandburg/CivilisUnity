611d54fdcada743e26ffbc1fd453c1b66b241546
Justin Strandburg <justin.strandburg@hotmail.com>
2016-08-01 23:08:44 -0700

    Added a monobehaviour to align child meshes with terrain

---
+++ Assets/Scripts/MB/AlignChildrenWithTerrain.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlignChildrenWithTerrain : MonoBehaviour {
+
+    private Vector3 origin;//our current position as snapped to ground
+
+	// Use this for initialization
+	void Start () {
+        origin = GameController.instance.SnapToGround(transform.position);
+        foreach (Transform child in transform) {
+            if (child.GetComponent<MeshRenderer>()) {
+                AlignChild(child);
+            }
+        }
+	}
+
+    private void AlignChild(Transform child) {
+        var relativeSnappedPosition = GameController.instance.SnapToGround(child.position);
+        child.position += new Vector3(0.0f, relativeSnappedPosition.y - origin.y, 0.0f);
+    }
+}
