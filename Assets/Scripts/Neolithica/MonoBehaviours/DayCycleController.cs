9b090df867d660fcc6d382cd4c4586157722124a
Justin Strandburg <justin.strandburg@hotmail.com>
2017-03-17 22:06:38 -0700

    Fix DayCycleController

--- Assets/Scripts/Neolithica/MonoBehaviours/DayCycleController.cs
+++ Assets/Scripts/Neolithica/MonoBehaviours/DayCycleController.cs
@@ -45,7 +45,7 @@ namespace Neolithica.MonoBehaviours {
             if (!Physics.Raycast(Camera.main.transform.position, ray.direction, out hit, Mathf.Infinity, terrainLayerMask))
                 return;
 
-            const float nightLightFloatDistance = 10.0f;
+            const float nightLightFloatDistance = 7.75f;
             nightLight.transform.position = new Vector3(
                 hit.point.x,
                 Mathf.Max(0.0f, hit.point.y) + nightLightFloatDistance,
